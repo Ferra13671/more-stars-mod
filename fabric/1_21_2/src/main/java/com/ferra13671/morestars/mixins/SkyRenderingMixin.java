7b394d5e15dde15fa1b55c9b30b4c4f4c1f82f86
Ferra13671 <ferra13671@gmail.com>
2026-02-09 19:01:32 +0300

    >= 1.21.2 fabric version

---
+++ fabric/1_21_2/src/main/java/com/ferra13671/morestars/mixins/SkyRenderingMixin.java
@@ -0,0 +1,20 @@
+package com.ferra13671.morestars.mixins;
+
+import net.minecraft.client.render.SkyRendering;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(SkyRendering.class)
+public class SkyRenderingMixin {
+
+    @ModifyConstant(method = "tessellateStars", constant = @Constant(intValue = 1500))
+    public int modifyStarsConstantInTessellateStars(int constant) {
+        return 20000;
+    }
+
+    @ModifyConstant(method = "tessellateStars", constant = @Constant(floatValue = 0.15f))
+    public float modifyStarSizeConstantInTessellateStars(float constant) {
+        return 0.05f;
+    }
+}
